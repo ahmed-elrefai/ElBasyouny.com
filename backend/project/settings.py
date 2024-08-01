4c49078c2462536bdd0a11758774fba550d2a427
Ahmed Elrefai <114250600+ahmed-elrefai@users.noreply.github.com>
2024-08-01 22:56:51 +0300

    securing some creds

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -156,7 +156,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'elrefaayahmed196@gmail.com'
-EMAIL_HOST_PASSWORD = 'vvjt amno zbla mswu'
+EMAIL_HOST_PASSWORD = '...'
 
 # logging config:
 
